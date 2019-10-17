b01324fa9d68c99850004f3991d4d45d7a00aa79
FernandoPicazo <fernando.picazo@outlook.com>
2019-10-17 14:34:19 -0500

    Rename source to sfdc_api

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 setup(name='sfdc_api',
       version='0.1',
-      packages=['src'],
+      packages=['sfdc_api'],
       license='MIT',
       long_description='')
