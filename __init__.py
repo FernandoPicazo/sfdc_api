6baca3af13a18247fe6d3304d8656cc765fe7c84
FernandoPicazo <fernando.picazo@outlook.com>
2019-10-17 19:50:15 -0500

    Prepare sfdc_api for addition to pypi

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .sfdc_api import Session
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 # from distutils.core import setup
 from setuptools import setup, find_packages
 setup(name='sfdc_api',
-      version='1.0',
+      version='0.1',
       packages=find_packages(),
       license='MIT',
+      download_url = 'https://github.com/FernandoPicazo/sfdc_api.git',
       long_description='')
