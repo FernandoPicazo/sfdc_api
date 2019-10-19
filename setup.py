0b0ec6ebba0c3227d7d14d8531f491c3584f26c6
FernandoPicazo <fernando.picazo@outlook.com>
2019-10-19 14:58:01 -0500

    Added developmental release versions\n added build relics to gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # from distutils.core import setup
 from setuptools import setup, find_packages
 setup(name='sfdc_api',
-      version='0.1',
+      version='0.1dev0',
       packages=find_packages(),
       license='MIT',
       download_url = 'https://github.com/FernandoPicazo/sfdc_api.git',
